00ccd3c191c4b0e93ebbfc0f514dc202724f2144
g0su12 <huy5763@gmail.com>
2023-04-04 20:54:13 +0700

    API delete Student, add Student

--- js/api/admin.js
+++ js/api/admin.js
@@ -2,6 +2,10 @@ const saveInfo = (btn) => {
     localStorage.setItem("endpoint", btn.getAttribute("id"));
 }
 
+function reverse(s){
+    return s.split("-").reverse().join("/");
+}
+
 const getNamHoc = () => {
     const APIUrl = "http://localhost:8080/api/v1/admin/nam-hocs";
     fetch(APIUrl, {
@@ -13,8 +17,9 @@ const getNamHoc = () => {
         let render = data.map((item, index) => {
             return `<li >
                         <button 
-                            id="${item.ten}" 
+                            class="button" 
                             onclick=saveInfo(this)
+                            id="${item.ten}" 
                         >
                             <a href="./ky-hoc.html">${item.ten}</a>
                         </button>
@@ -35,7 +40,10 @@ const getKyHoc = () => {
     .then((data) => {
         let render = data.map((item, index) => {
             return `<li>
-                        <button onclick="saveInfo(this)" id="${item.kyHoc.ten.split(" ").join("-")}*${item.namHoc.ten}">
+                        <button 
+                            class="button" 
+                            onclick="saveInfo(this)" 
+                            id="${item.kyHoc.ten.split(" ").join("-")}*${item.namHoc.ten}">
                             <a href="./nien-khoa.html">${item.kyHoc.ten}</a>
                         </button>
                     </li>`;
@@ -55,7 +63,10 @@ const getNienKhoa = () => {
     .then((data) => {
         let render = data.map((item, index) => {
             return `<li>
-                        <button onclick="saveInfo(this)" id=${item.ten}*${localStorage.getItem("endpoint")}>
+                        <button 
+                            class="button"
+                            onclick="saveInfo(this)" 
+                            id=${item.ten}*${localStorage.getItem("endpoint")}>
                             <a href="./nganh-hoc.html">${item.ten}</a>
                         </button>
                     </li>`;
@@ -75,7 +86,10 @@ const getNganhHoc = () => {
     .then((data) => {
         let render = data.map((item, index) => {
             return `<li>
-                        <button onclick="saveInfo(this)" id="${item.ten.split(" ").join("-")}*${localStorage.getItem("endpoint")}">
+                        <button 
+                            class="button"
+                            onclick="saveInfo(this)" 
+                            id="${item.ten.split(" ").join("-")}*${localStorage.getItem("endpoint")}">
                             <a href="./mon-hoc.html">${item.ten}</a>
                         </button>
                     </li>`;
@@ -87,6 +101,7 @@ const getNganhHoc = () => {
 const getMonHoc = () => {
     const paths = localStorage.getItem("endpoint").split("*");
     const APIUrl = `http://localhost:8080/api/v1/admin/nam-hocs/${paths[3]}/ky-hocs/${paths[2]}/nien-khoas/${paths[1]}/nganh/${paths[0]}/mon-hocs`;
+
     fetch(APIUrl, {
         method: 'GET',
         headers: {'content-type':'application/json'},
@@ -96,7 +111,9 @@ const getMonHoc = () => {
         let render = data.map((item, index) => {
             return `<tr>
                         <td>${item.id}</td>
-                        <td onclick="saveInfo(this)" id="${item.ten.split(" ").join("-")}*${localStorage.getItem("endpoint")}">
+                        <td 
+                            onclick="saveInfo(this)" 
+                            id="${item.ten.split(" ").join("-")}*${localStorage.getItem("endpoint")}">
                             <a href="./lop-hoc.html">${item.ten.toLowerCase().replace(/(^|\s)\S/g, l => l.toUpperCase())}</a>
                         </td>
                         <td>${item.tyLeDiemCC}</td>
@@ -112,6 +129,8 @@ const getMonHoc = () => {
 
 const getLHP = () => {
     const paths = localStorage.getItem("endpoint").split("*");
+    document.querySelector("#sub-name").innerHTML = 
+        paths[0].split("-").join(" ").toLowerCase().replace(/(^|\s)\S/g, l => l.toUpperCase());
     const APIUrl = `http://localhost:8080/api/v1/admin/nam-hocs/${paths[4]}/ky-hocs/${paths[3]}/nien-khoas/${paths[2]}/nganh/${paths[1]}/mon-hocs/${paths[0]}/lhps`;
     fetch(APIUrl, {
         method: 'GET',
@@ -121,14 +140,51 @@ const getLHP = () => {
     .then((data) => {
         let render = data.map((item, index) => {
             return `<tr>
-                        <td onclick="saveInfo(this)" id="${item.ten.split(" ").join("-")}*${localStorage.getItem("endpoint")}">
-                            <a href="./lop-hoc.html">${item.ten}</a>
+                        <td>${item.id}</td>
+                        <td onclick="saveInfo(this)" id="${item.id}*${localStorage.getItem("endpoint")}">
+                            <a href="./ds-sv.html">${item.nienKhoaNganhNamHocKyHocMonHoc.monHoc.ten.toLowerCase().replace(/(^|\s)\S/g, l => l.toUpperCase())}</a>
                         </td>
-                        <td>${item.tyLeDiemCC}</td>
-                        <td>${item.tyLeDiemTH}</td>
-                        <td>${item.tyLeDiemKT}</td>
-                        <td>${item.tyLeDiemBT}</td>
-                        <td>${item.tyLeDiemCuoiKy}</td>
+                        <td>${item.giaoVien.nguoiDung.name.toLowerCase().replace(/(^|\s)\S/g, l => l.toUpperCase())}</td>
+                        <td>${item.phongHoc.ten}</td>
+                        <td>${item.listKetQua.length}</td>
+                        <td>${reverse(item.batDau.toLocaleString('en-CA').slice(0, 10))}</td>
+                        <td>${reverse(item.ketThuc.toLocaleString('en-CA').slice(0, 10))}</td>
+                    </tr>`;
+        });
+        document.querySelector("#main").innerHTML = render.join("");
+    });
+}
+
+const getDSSV = () => {
+    const paths = localStorage.getItem("endpoint").split("*");
+    // document.querySelector("#sub-name").innerHTML = paths[0].split("-").join(" ").toLowerCase().replace(/(^|\s)\S/g, l => l.toUpperCase());
+    const APIUrl = `http://localhost:8080/api/v1/admin/nam-hocs/${paths[5]}/ky-hocs/${paths[4]}/nien-khoas/${paths[3]}/nganh/${paths[2]}/mon-hocs/${paths[1]}/lhps/${paths[0]}/sinh-viens`;
+    fetch(APIUrl, {
+        method: 'GET',
+        headers: {'content-type':'application/json'},
+    })
+    .then((response) => response.json())
+    .then((data) => {
+        let render = data.map((item, index) => {
+            return `<tr>
+                        <td>${item.id}</td>
+                        <td>${item.nguoiDung.name.toLowerCase().replace(/(^|\s)\S/g, l => l.toUpperCase())}</td>
+                        <td>${item.maSinhVien}</td>
+                        <td>${item.nguoiDung.gioiTinh.toLowerCase().replace(/(^|\s)\S/g, l => l.toUpperCase())}</td>
+                        <td>${item.nguoiDung.soDienThoai}</td>
+                        <td>${item.nguoiDung.email}</td>
+                        <td>${item.nguoiDung.diaChi.toLowerCase().replace(/(^|\s)\S/g, l => l.toUpperCase())}</td>
+                        <td>
+                            <button class="edit" onclick="editInfoStudent(this)">Sửa thông tin</button>
+                            <button class="delete" id="${item.id}" onclick="showForm(${item.id})">Xoá sinh viên</button>
+                        </td>
+                        <div class="toggle" id="delete-${item.id}">
+                            <div class="form-msg">
+                            <h4 class="form-title">Xác nhận xoá sinh viên</h4>
+                                <button onclick="deleteStudent(${item.id})" class="submit">Xác nhận</button>
+                                <button onclick="showForm(${item.id})" class="submit">Huỷ</button>
+                            </div>
+                        </div>
                     </tr>`;
         });
         document.querySelector("#main").innerHTML = render.join("");
